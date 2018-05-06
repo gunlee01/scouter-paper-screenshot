4d242923e562eeeb6a1dd717d2ca591f4ca9134e
Gunhee Lee <gunlee01@gmail.com>
2018-05-06 17:23:42 +0900

    puppeteer no sandbox

--- module/shooter.js
+++ module/shooter.js
@@ -5,8 +5,10 @@ const log = require('./log');
 const shooter = {};
 
 shooter.makeScreenShot = async function(url, outDir, fileName) {
+    log.info(`[screenshot cooking start] : ${fileName}`);
+    
     const start = moment();
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox']});
     const page = await browser.newPage();
     try {
         await page.setViewport({width: 480, height: 720,});
@@ -18,12 +20,11 @@ shooter.makeScreenShot = async function(url, outDir, fileName) {
             fullPage: true
         });
         await browser.close();
+        log.info(`[screenshot worked] : ${fileName}, elapsed: ${moment.duration(moment().diff(start)).asSeconds()}`);
 
     } catch (e) {
-        log.error(`[upload oss error] body : ${JSON.stringify(e)}`, e);
+        log.error(`[puppeteer error on making scrrenshot] ${JSON.stringify(e)}`, e);
     }
-
-    log.info(`[screenshot worked] : ${fileName}, elapsed: ${moment.duration(moment().diff(start)).asSeconds()}`);
 };
 
 
