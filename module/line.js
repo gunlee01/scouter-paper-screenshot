e1b9e7d84f3972e2568cba6886a6e5b54cde134b
Gunhee Lee <gunlee01@gmail.com>
2020-03-30 19:19:42 +0900

    use line notify api

--- module/line.js
+++ module/line.js
@@ -1,6 +1,7 @@
 const https = require('https');
 const config = require('config');
 const log = require('./log');
+const request = require('request');
 
 const line = {};
 
@@ -16,37 +17,59 @@ line.send = function (formData) {
     //     messages: messages
     // });
 
-    const options = {
-        hostname: config.get('line.hostname'),
-        port: 443,
-        path: config.get('line.push-path'),
-        method: 'POST',
+    const apiUrl = config.get('line.hostname') + config.get('line.push-path');
+
+    request.post({
+        url: apiUrl,
         headers: {
             'Content-Type': 'multipart/form-data',
             'Authorization': 'Bearer ' + config.get('line.token')
         },
-        formData : {
-            'message' : formData.message,
+        form: {
+            'message': formData.message,
             'imageThumbnail': formData.imageThumbnail,
             'imageFullsize': formData.imageFullsize
         }
-    };
-
-    const postReq = https.request(options, function (postRes) {
-        log.info(`[line response] status : ${JSON.stringify(postRes.statusCode)}`);
-        log.info(`[line response] HEADERS : ${JSON.stringify(postRes.headers)}`);
-
-        postRes.on('data', function (chunk) {
-            log.info(`[line response] body : ${chunk}`);
-        })
+    }, function (err, httpResponse, body) {
+        if (err) {
+            log.info(`[line response] httpResponse : ${JSON.stringify(httpResponse)}`);
+            log.info(`[line response] body : ${body}`);
+        } else {
+            log.error(`[line call error] body : ${JSON.stringify(err)}`, err);
+        }
     });
 
-    postReq.on('error', function (e) {
-        log.error(`[line call error] body : ${JSON.stringify(e)}`, e);
-    });
-
-    postReq.write(postData);
-    postReq.end();
+    // const options = {
+    //     hostname: config.get('line.hostname'),
+    //     port: 443,
+    //     path: config.get('line.push-path'),
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'multipart/form-data',
+    //         'Authorization': 'Bearer ' + config.get('line.token')
+    //     },
+    //     formData : {
+    //         'message' : formData.message,
+    //         'imageThumbnail': formData.imageThumbnail,
+    //         'imageFullsize': formData.imageFullsize
+    //     }
+    // };
+    //
+    // const postReq = https.request(options, function (postRes) {
+    //     log.info(`[line response] status : ${JSON.stringify(postRes.statusCode)}`);
+    //     log.info(`[line response] HEADERS : ${JSON.stringify(postRes.headers)}`);
+    //
+    //     postRes.on('data', function (chunk) {
+    //         log.info(`[line response] body : ${chunk}`);
+    //     })
+    // });
+    //
+    // postReq.on('error', function (e) {
+    //     log.error(`[line call error] body : ${JSON.stringify(e)}`, e);
+    // });
+    //
+    // postReq.write();
+    // postReq.end();
 };
 
 module.exports = line;
